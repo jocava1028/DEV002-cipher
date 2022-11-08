e3d88680543b26d433bd80ea6d7fc19a7afe0a06
Johanna <jocava1028@gmail.com>
2022-11-08 00:13:09 -0300

    se hizo algoritmo en cipher js

--- src/cipher.js
+++ src/cipher.js
@@ -2,11 +2,24 @@ const cipher = {
 funcionCifrar: (texto, numero) =>{
   // texto es el mensaje que escribe el usuario
   //numero es el deplazamiento que elije el usuario
-  console.log("entro a cipher", texto, numero)
+  console.log("entro a cipher", texto, numero);
+    if (!texto) return "";
+    const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    numero = (numero % 26 + 26) %26;
+    console.log(texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+numero)%26]))
+    return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+numero)%26]);
+    
 },
-funcionDescifrar: () =>{
+
+funcionDescifrar: (texto, numero) =>{
+    if (!texto)return "";
+    const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    numero = (numero % 26 - 26) %26;
+    console.log(texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+numero)%26]))
+    return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)-numero)%26]);
 
 }
-};
+}
+
 
 export default cipher;
