1f5ea490bf81ed69e150c854abbc2fbe588f2117
Johanna <jocava1028@gmail.com>
2022-11-10 19:53:01 -0300

    se comenta el codigo

--- src/index.js
+++ src/index.js
@@ -9,37 +9,48 @@ const botonCifrar = document.getElementById("boton-cifrar");
 const botonDescifrar = document.getElementById("boton-descifrar");
 
 
-// le doy funcionalidad 
-let offset
-let msjCifrado
-let msjDescifrado
-let resultadoCifrado
-let resultadoDescifrado
+// declaro las variable que van a guardar los valores mas importantes
+let offset // desplazamiento elegido por el usuario
+let msjCifrado // mensaje que escribe el usuario
+let msjDescifrado // mensaje que escribe el usuario
+let resultadoCifrado //es el resultado de la funcion encode
+let resultadoDescifrado //es el resultado de la funcion decode
 
-botonCifrar.addEventListener("click", () => {
+botonCifrar.addEventListener("click", () => { // se le agrega el evento click al boton cifrar
+
     resultadoCifrado = cipher.encode(offset, msjDescifrado) // llamar al metodo cifrar del objeto cipher
-    if (!msjCifrado || msjCifrado === "") {
-        inputCifrado.value = resultadoCifrado
-    }
+    
+        inputCifrado.value = resultadoCifrado // se le asigna el resultado cifrado para que se vea en el input cifrado
+    
 })
 botonDescifrar.addEventListener("click", () => {
     resultadoDescifrado = cipher.decode(offset, msjCifrado) // llamar al metodo descifrar del objeto cipher
-    if (!msjDescifrado || msjDescifrado === "") {
-        inputDescifrado.value = resultadoDescifrado
-    }
+   
+        inputDescifrado.value = resultadoDescifrado // se le asigna el resultado para que se vea en input descifrado
+    
 
 })
 
 
-inputNumerico.addEventListener("change", (event) => {
+inputNumerico.addEventListener("change", (event) => { // se le agrega un escuchador al evento change en el input numerico
+    // (desplazaminto)
+    
 
-    offset = event.target.value
+    offset = Number(event.target.value) // se guarda en la variable offset el desplazamiento elegido por el usuario
+    // y se tranforma a numero
 })
-inputCifrado.addEventListener("keyup", (event) => {
+inputCifrado.addEventListener("keyup", (event) => { // se agrega el escuchador al evento keyup 
+    // (cuando se deja de presionar una tecla)
+   
+   event.target.value= event.target.value.toUpperCase(); // cada letra que el usuario escribe se convierte en mayuscula
 
-    msjCifrado = event.target.value
+
+    msjCifrado = event.target.value; // guarda elmensaje escrito por el usuario  en input cifrado en msj cifrado
 })
-inputDescifrado.addEventListener("keyup", (event) => {
+inputDescifrado.addEventListener("keyup", (event) => {// se agrega el escuchador al evento keyup 
+    // (cuando se deja de presionar una tecla)
+    event.target.value= event.target.value.toUpperCase(); //cada letra que el usuario escribe se convierte en mayuscula
 
-    msjDescifrado = event.target.value
+
+    msjDescifrado = event.target.value;
 })
